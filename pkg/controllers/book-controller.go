99914229e312271514464ece5e96e3586c5402d8
atanda nafiu <atandanafiu21@gmail.com>
2023-01-24 23:58:33 +0100

    commit

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/atanda0x/bookstore/models"
+	"github.com/atanda0x/bookstore/pkg/models"
+	"github.com/atanda0x/bookstore/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["book"]
+	ID, err := strconv.ParseInt(bookId, 0.0)
+	if err != nil {
+		fmt.Fprintln("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
